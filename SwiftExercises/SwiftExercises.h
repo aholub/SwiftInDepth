d38e2775a1791f6ac4e75992b0373beff78fed89
Allen Holub <allen@holub.com>
2015-09-21 16:14:25 -0700

    Initial Commit

---
+++ SwiftExercises/SwiftExercises.h
@@ -0,0 +1,19 @@
+//
+//  SwiftExercises.h
+//  SwiftExercises
+//
+//  Created by allen on 9/21/15.
+//  Copyright © 2015 allen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftExercises.
+FOUNDATION_EXPORT double SwiftExercisesVersionNumber;
+
+//! Project version string for SwiftExercises.
+FOUNDATION_EXPORT const unsigned char SwiftExercisesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftExercises/PublicHeader.h>
+
+
